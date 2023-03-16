2082ccb3ec39fe8cccf4bdebfd6546c47aca3b63
Arman19941113 <td19941113@gmail.com>
2023-03-16 19:59:31 +0800

    build: update config

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,9 +1,6 @@
-import { nodeResolve } from '@rollup/plugin-node-resolve'
 import typescript from '@rollup/plugin-typescript'
-import replace from '@rollup/plugin-replace'
-import terser from '@rollup/plugin-terser'
 
-const devTasks = {
+export default {
   input: 'src/index.ts',
   external: ['axios'],
   output: [
@@ -17,44 +14,6 @@ const devTasks = {
     },
   ],
   plugins: [
-    nodeResolve(),
     typescript(),
-    replace({
-      preventAssignment: true,
-      values: {
-        'process.env.NODE_ENV': JSON.stringify('development'),
-      },
-    }),
   ],
 }
-
-const prodTasks = {
-  input: 'src/index.ts',
-  external: ['axios'],
-  output: [
-    {
-      file: 'dist/index.cjs.prod.js',
-      format: 'cjs',
-    },
-    {
-      file: 'dist/index.esm.prod.js',
-      format: 'es',
-    },
-  ],
-  plugins: [
-    nodeResolve(),
-    typescript(),
-    replace({
-      preventAssignment: true,
-      values: {
-        'process.env.NODE_ENV': JSON.stringify('production'),
-      },
-    }),
-    terser(),
-  ],
-}
-
-export default [
-  devTasks,
-  prodTasks,
-]
