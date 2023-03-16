1d6b27251ce9d249bb01e217aa6a6db14a66515a
Arman19941113 <td19941113@gmail.com>
2023-03-16 20:53:52 +0800

    build: update config

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,4 +1,6 @@
+import replace from '@rollup/plugin-replace'
 import typescript from '@rollup/plugin-typescript'
+import { nodeResolve } from '@rollup/plugin-node-resolve'
 
 export default {
   input: 'src/index.ts',
@@ -14,6 +16,17 @@ export default {
     },
   ],
   plugins: [
-    typescript(),
+    replace({
+      preventAssignment: true,
+      values: {
+        'process.env.NODE_ENV': JSON.stringify('production'),
+      },
+    }),
+    typescript({
+      compilerOptions: {
+        removeComments: true,
+      },
+    }),
+    nodeResolve(),
   ],
 }
